194b96917956542568f3ef59cc036a1517f6fca0
Ravi Vendra R <ravi.vendra.rishika@gmail.com>
2021-03-25 23:47:18 +0700

    First commit for this switching/gateway simulator

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # codeigniter-gateway-simulator
+
+CodeIgniter switching simulator, for testing purpose on my personal project.
---
+++ src/controllers/Gateway.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gateway extends CI_Controller {
+	private $data;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->helpers(['url']);
+	}
+
+	public function index()
+	{
+		redirect('gateway/inquiry');
+	}
+
+	public function inquiry() {
+		$this->data = [
+			'statusCode'			=> '00',
+			'accountNo'				=> '1035055604545',
+			'accountName'			=> 'D A N R A',
+			'reference'				=> date('ymdHis') . mt_rand(100, 999),
+			'message'				=> 'Success',
+			'stan'					=> (string) mt_rand(100000, 999999)
+		];
+
+		$this->output
+			->set_content_type('application/json')
+			->set_header('HTTP/1.1 200 OK')
+			->set_header('Cache-Control: no-store, no-cache, must-revalidate')
+			->set_header('Pragma: no-cache')
+			->set_status_header(200)
+			->set_output(json_encode($this->data), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	}
+
+	public function request() {
+		$this->data = [
+			'statusCode'			=> '00',
+			'virtualAccountNumber'	=> '1035055604545',
+			'accountNo'				=> '1035055604545',
+			'accountName'			=> 'D A N R A',
+			'accountCbc'			=> '008',
+			'amount'				=> 10000,
+			'message'				=> 'Success',
+			'reference'				=> date('ymdHis') . mt_rand(100, 999),
+			'altoRef'				=> date('ymdHis') . mt_rand(100, 999),
+			'stan'					=> (string) mt_rand(100000, 999999)
+		];
+
+		$this->output
+			->set_content_type('application/json')
+			->set_header('HTTP/1.1 200 OK')
+			->set_header('Cache-Control: no-store, no-cache, must-revalidate')
+			->set_header('Pragma: no-cache')
+			->set_status_header(200)
+			->set_output(json_encode($this->data), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	}
+}
